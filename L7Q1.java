1980ec0a797d6ffba6c526f52ddc67091bf8a955
OtakuSensei04 <150838831+OtakuSensei04@users.noreply.github.com>
2023-12-08 21:12:54 +0400

    Create L7Q1.java
    Write a java program to store information for a showroom that has a class name a Car with attributes:
    Vehicle_id: String
    Cost: Float[]; stores base, exercise duty and sales tax for each car
    Total: float; stores total after calling calc_total
    Grand_total: float; stores price after subtracting discount
    calc_total() calculates total price (Base + Exercise duty + Sales tax)
    calc_grand_total() calculates the price after a discount of a 10% Diwali discount.
    In main method, Create an array of objects of Car.
    Now call calc_total() to calculate total. Then after call calc_grand_total() to calculate the price after the discount.
    Now Create a method which prints the details of the car with the highest grand price.
    Note the array of object store the detail of n number of employees.

---
+++ L7Q1.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+  class Car
+  {
+       String Vehicle_id;
+       float[] Cost;
+       float Total=0;
+       float Grand_total;
+      
+      void calc_total(String Vehicle_id,float[] Cost)
+      {
+          for(int j=0;j<3;j++)
+          this.Total=this.Total+Cost[j];
+      }
+      void calc_grand_total()
+      {
+          this.Grand_total=this.Total*0.1f;
+          
+      }
+      
+  }
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter no. of employees: ");
+	    int n=sc.nextInt();
+	    Car[] obj=new Car[n];
+	    Main cr=new Main();
+	     String Vehicle_id;
+         float[] Cost;
+        // float Total;
+        // float Grand_total;
+         int i=0,j=0;
+	    for(i=0;i<n;i++)
+	    {
+	        obj[i]=new Car();
+	        System.out.println("Enter Vehicle id: ");
+	         sc.nextLine();
+	        Vehicle_id=sc.nextLine();
+	       
+	        System.out.println("Enter Vehicle's base, exercise duty and sales tax: ");
+	        Cost = new float[3];
+	        for(j=0;j<3;j++)
+	        {
+	        Cost[j]=sc.nextFloat();
+	        }
+	        obj[i].calc_total(Vehicle_id,Cost);
+	        obj[i].calc_grand_total();
+	    }
+	   
+            int hh=cr.highest_grand_price(obj,n);
+          System.out.println("Highest Grand Total: "+obj[hh].Grand_total);
+          System.out.println(" Vehicle id: "+obj[hh].Vehicle_id);
+	        
+	        for(int p=0;p<3;p++)
+	        {
+	        System.out.println(" Vehicle's base, exercise duty and sales tax: "+obj[hh].Cost[p]);
+	        }
+      
+    }
+     int highest_grand_price(Car[] obj,int n)
+      {
+          
+          int ff=0;
+          float max=obj[0].Grand_total;
+          for(int k=0;k<n-1;k++)
+          {
+              if(obj[k].Grand_total<obj[k+1].Grand_total)
+              {
+              max=obj[k+1].Grand_total;
+              ff=k+1;
+              }
+          }
+          return ff;
+      }
+}
