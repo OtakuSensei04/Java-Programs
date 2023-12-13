9e482af149008ec9f2bc2cb04ffc20026f711672
OtakuSensei04 <150838831+OtakuSensei04@users.noreply.github.com>
2023-12-13 12:35:13 +0400

    Update L8Q1.java
    Finally did it

--- L8Q1.java
+++ L8Q1.java
@@ -1,79 +1,73 @@
-import java.util.*; 
+import java.util.*;
 class FlightDest
 {
     private String flightNo,destination;
-    FlightDest(String fno,String dest)
+    FlightDest(String flightNo,String destination)
     {
-        this.flightNo=fno;
-        this.destination=dest;
+        this.flightNo=flightNo;
+        this.destination=destination;
     }
-    String getDest() 
-    {
-        return destination;
-    }
-    String getFlightNo()
-    {
-        return flightNo;
-    }
+    String getflightNo()
+    { return flightNo; }
+    
+    String getdestination()
+    { return destination; }
 }
 class FlightInfo
 {
     ArrayList<FlightDest> flightDestList=new ArrayList<FlightDest>();
+    
     public FlightInfo()
-    {
-        
-    }
+    {}
     public void addFlightDestPair(String fNo,String dest)
     {
         flightDestList.add(new FlightDest(fNo,dest));
     }
-    public ArrayList<String> getFlights(String dest)
+    public ArrayList<String> getFlightsDest(String dest)
     {
-        ArrayList<String> flights=new ArrayList<String>();
-        for(FlightDest fly:  flightDestList )
-        if(fly.getDest().equals(dest))
+        ArrayList<String> fly=new ArrayList<String>();
+        for(FlightDest d:flightDestList )
+        if(d.getdestination().equals(dest))
         {
-            flights.add(fly.getFlightNo());
+             fly.add(d.getflightNo());
         }
-        return flights;
+        return fly;
     }
 }
-public class Main {
-
-    public static void main(String args[]) {
-
-        int choice = 0;
-        FlightInfo fi = new FlightInfo();
-
-        Scanner sc = new Scanner(System.in);
-
-        while (choice != 3) {
-
-            System.out.println("Enter choice:\n1. Add Flight-Destination Pair\n2. Get Flights for Destination\n3. Exit\n");
-            choice = Integer.parseInt(sc.nextLine());
-
-            switch (choice) {
-
-                case 1: {
-                    System.out.println("Enter Flight and Destination separately");
-                    fi.addFlightDestPair(sc.nextLine(), sc.nextLine());
-                    break;
-                }
-
-                case 2: {
-                    System.out.print("Enter Destination = ");
-                    System.out.println(fi.getFlights(sc.nextLine()));
-                    break;
-                }
-
-            }
-
-            System.out.println("");
-
-        }
-
-        sc.close();
-
-    }
-
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    FlightInfo obj=new FlightInfo();
+		System.out.println("Enter 1 to add fno and dest");
+		System.out.println("Enter 2 to get flight for ur dest");
+		System.out.println("Enter 3 to exit");
+		int n=sc.nextInt();
+		sc.nextLine();
+		while(n!=3)
+		{
+		    switch(n)
+		    {
+		        case 1:
+		            System.out.println("Enter fno and dest");
+		            String fl=sc.nextLine();
+		            String des=sc.nextLine();
+		            obj.addFlightDestPair(fl,des);
+		            break;
+		        case 2:
+		            System.out.println("Enter dest to get flight no.");
+		            String d=sc.nextLine();
+		            System.out.println(obj.getFlightsDest(d));
+		            break;
+		        default:
+		            System.out.println("Invalid input");
+		    }
+		    System.out.println("Enter 1 to add fno and dest");
+		System.out.println("Enter 2 to get flight for ur dest");
+		System.out.println("Enter 3 to exit");
+		    n=sc.nextInt();
+		    sc.nextLine();
+		}
+	}
 }
